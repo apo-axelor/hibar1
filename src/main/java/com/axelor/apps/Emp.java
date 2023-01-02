bc24f149f326d9c991290def88c768935354f206
apo-axelor <a.popat@axelor.com>
2023-01-02 15:59:44 +0530

    sucess

--- src/main/java/com/axelor/apps/Emp.java
+++ src/main/java/com/axelor/apps/Emp.java
@@ -1,22 +1,16 @@
 package com.axelor.apps;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 
-@Entity
 public class Emp{
-	@Id
-	@GeneratedValue
 	private int id;
 	private String name;
 	
+	public void setid(int id) {
+		this.id =id;
+	} 
 	public int getId() {
 		return id;
 	}
-	
-
-
 	
 	public String getName() {
 		return name;
--- src/main/java/com/axelor/apps/main/App.java
+++ src/main/java/com/axelor/apps/main/App.java
@@ -1,7 +1,8 @@
-
- 
 package com.axelor.apps.main;
 
+
+
+import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -10,38 +11,54 @@ import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.query.Query;
 
 import com.axelor.apps.Emp;
 
+
 public class App {
+
 	public static void main(String[] args) {
-		 StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();  
-         Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();  
-         
-         SessionFactory factory = meta.getSessionFactoryBuilder().build();  
-         Session session = factory.openSession(); 
-         System.out.println("Fetching object using get:");
-         Emp s=session.get(Emp.class, 7);
-         System.out.println("Id : "+s.getId());
-         System.out.println("Name : "+s.getName());
-         
-         Emp s1=session.get(Emp.class, 7);
-         s1.setName("prachi");
-         session.save(s);
-         
-        Emp s2=session.get(Emp.class, 8);
-        session.delete(s2);
- 
-         Transaction t = session.beginTransaction();  
+		// TODO Auto-generated method stub
+		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();  
+        Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();  
+        
+        SessionFactory factory = meta.getSessionFactoryBuilder().build();  
+        Session session = factory.openSession(); 
+        Transaction t = session.beginTransaction(); 
+                 
+
+        //Emp e1 = new Emp();
+        //e1.setName("test");
+        //session.save(e1);
+        //t.commit();//transaction is commited  
+        //session.close();  
+          
+        //System.out.println("successfully saved"); 
+        //int id = 1;
+	//Emp s1 = session.get(Emp.class, new Integer(id));
+		//session.delete(s1);
+		//session.getTransaction().commit();
+		//System.out.println("record deleted.....");
+		//int id = 2;
+	//Emp s1 = session.get(Emp.class,new Integer(id));
+		//s1.setName("sukh");	
+		//session.saveOrUpdate(s1);
+	//session.getTransaction().commit();
+		//System.out.println("Record updated succesfully..."); 
+	  
+        Query query = session.createQuery("FROM Emp");
+		List<Emp> e1 = query.list();
+		System.out.println("-------------Records are -------------");
+		for(Emp empObj : e1)
+		{
+			System.out.print("\n emp Id  " + empObj.getId() + " \n empname : " + empObj.getName() + "\n");
+		}
+		System.out.print("");
+		    
 		
-         Emp e1 = new Emp();
-         e1.setName("mahek");
-         session.save(e1);  
-         t.commit();   
-         System.out.println("successfully saved");    
-          factory.close();  
-          session.close();    
-          
-          
-	}
+        
+        
+        
+}
 }
